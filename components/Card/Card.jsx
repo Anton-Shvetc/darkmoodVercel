ecd026728e662a23fde86feb52bdf695a9e4a07b
Anton <antonshvetswork@yandex.ru>
2023-10-04 09:22:37 +0300

    Добавлена базовая верстка для компонента карточи каталога

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,9 +1,22 @@
-import styles from './Card.module.scss';
-
-export const Card = ({ name, price }) => {
+import styles from "./Card.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+export const Card = ({ name, price, imageUrl }) => {
   return (
     <>
-      <div className={styles.card}>Карточка {name}</div>
+      <div className={styles.card}>
+        <Link href="#" className={styles.card__moreLink}>
+          <div className={styles.card__price}>
+           
+            {price} USD
+          </div>
+          <div className={styles.card__img}>
+            <span>Подробнее</span>
+            <Image src={imageUrl} width="100%" height="100%" alt={name} />
+          </div>
+        </Link>
+        <button className={styles.card__button}>{name}</button>
+      </div>
     </>
   );
 };
--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -1,22 +1,26 @@
 import { Card } from "@/components/Card/Card";
 import styles from "./Cards.module.scss";
+import imageUrl from "@/public/images/card-img.png"
+
 export const Cards = () => {
   const mockData = [
-    { id: 1, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
-    { id: 2, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
-    { id: 3, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
-    { id: 4, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
-    { id: 5, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
-    { id: 6, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
-    { id: 7, name: "Футболка Darkmood", price: "49,99", imageUrl: "" },
+    {
+      id: 1,
+      name: "Футболка Darkmood",
+      price: "49,99",
+      imageUrl:  imageUrl ,
+    },
+    { id: 2, name: "Футболка Darkmood", price: "49,99", imageUrl: imageUrl },
+    { id: 3, name: "Футболка Darkmood", price: "49,99", imageUrl: imageUrl },
+
   ];
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.cards}>
         {mockData.map((card) => {
           return <Card key={card.id} {...card} />;
         })}
       </div>
-    </>
+    </div>
   );
 };
