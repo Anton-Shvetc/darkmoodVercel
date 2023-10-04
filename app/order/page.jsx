1f6aae600bd065f17522f48020c8a2940883cfc1
Anzhelika Shilyaeva <shilyaeva068@icloud.com>
2023-10-04 12:53:58 +0300

    add pages

---
+++ app/order/page.jsx
@@ -0,0 +1,9 @@
+import styles from './order.module.scss';
+
+export default function Order() {
+  return (
+    <div>
+      <h1>profile</h1>
+    </div>
+  );
+}
---
+++ app/questions/page.jsx
@@ -0,0 +1,9 @@
+import styles from './questions.module.scss';
+
+export default function Questions() {
+  return (
+    <div>
+      <h1>profile</h1>
+    </div>
+  );
+}
