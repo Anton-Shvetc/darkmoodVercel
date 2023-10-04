94dd0bd574fd1171f30293e8f7557dbd36ace400
Anzhelika Shilyaeva <shilyaeva068@icloud.com>
2023-10-04 12:39:10 +0300

    add links

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,16 +1,32 @@
+'use client';
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import User from '../../public/user.svg';
 import Cart from '../../public/cart.svg';
 import Logo from '../../public/logo.svg';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Header = () => {
+  const pathname = usePathname();
+
+  console.log(pathname);
   return (
     <header className={styles.root}>
       <ul>
-        <li>FAQ</li>
-        <li>ВАШ ЗАКАЗ</li>
-        <li>КАТАЛОГ</li>
+        <li>
+          <Link href="#">FAQ</Link>
+        </li>
+        <li>
+          <Link href="#">ВАШ ЗАКАЗ</Link>
+        </li>
+        <li>
+          <Link
+            href="/catalog"
+            className={`link ${pathname === '/' ? 'active' : ''}`}>
+            КАТАЛОГ
+          </Link>
+        </li>
       </ul>
       <Image
         className={styles.logo}
