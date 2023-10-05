9afe3781001c16e9e300956edc38b83c2011f749
Anton <antonshvetswork@yandex.ru>
2023-10-05 14:35:37 +0300

    Обновлены стили карточек товара

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,22 +1,47 @@
+"use client";
 import styles from "./Card.module.scss";
 import Image from "next/image";
 import Link from "next/link";
-import { ubuntuFont } from "@/app/layout";
 import classNames from "classnames";
+import arrowIcon from "@/public/icons/arrow-up.svg";
+import { Ubuntu } from "next/font/google";
 
+const ubuntuFont = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+});
 
 export const Card = ({ name, price, imageUrl }) => {
+
   return (
     <>
       <div className={classNames(`${ubuntuFont.variable}`, `${styles.card}`)}>
-        <Link href="#" className={styles.card__moreLink}>
+        <Link
+          href="#"
+          onClick={() => {
+            console.log("link");
+          }}
+          className={styles.card__moreLink}
+        >
+          <div className={styles.card__moreBtn}>
+            <span>Подробнее</span>
+            <Image src={arrowIcon} width={21} height={21} alt="arrow" />
+          </div>
+
           <div className={styles.card__price}>{price} USD</div>
           <div className={styles.card__img}>
-            <span>Подробнее</span>
             <Image src={imageUrl} width="427px" height="100%" alt={name} />
           </div>
         </Link>
-        <button className={styles.card__button}>{name}</button>
+        <button
+          onClick={() => {
+            console.log("click");
+          }}
+          className={styles.card__button}
+        >
+          {name}
+        </button>
       </div>
     </>
   );
