937c1d7a187154c4ba4a56ac9a1b5ab403c2f7fc
Anzhelika Shilyaeva <shilyaeva068@icloud.com>
2023-10-11 16:30:30 +0300

    add user page

--- app/profile/user/page.jsx
+++ app/profile/user/page.jsx
@@ -3,8 +3,26 @@ import styles from './user.module.scss';
 export default function User() {
   return (
     <div className={styles.user}>
-      <h1>Персональные даные</h1>
-      <h1>Адрес доставки</h1>
+      <div>
+        <h2>Персональные даные</h2>
+        <ul>
+          <li>ИМЯ</li>
+          <li>ФАМИЛИЯ</li>
+          <li>ОТЧЕСТВО</li>
+          <li>ТЕЛЕФОН</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Адрес доставки</h2>
+        <ul>
+          <li>СТРАНА</li>
+          <li>КРАЙ/ОБЛАСТЬ/РЕГИОН</li>
+          <li>ПОЧТОВЫЙ ИНДЕКС</li>
+          <li>ГОРОД</li>
+          <li>УЛИЦА, ДОМ, КВАРТИРА</li>
+        </ul>
+      </div>
     </div>
   );
 }
