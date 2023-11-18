997195cf537f739c67d6dec38d90947152cc3d28
Anton <antonshvetswork@yandex.ru>
2023-11-18 22:53:28 +0300

    Добавлена структура страницы авторизации

---
+++ app/(sample)/authorization/page.jsx
@@ -0,0 +1,54 @@
+
+'use client'
+import styles from "./authorization.module.scss";
+// import {RegisterForm} from "@/components/AuthForms/RegisterForm"
+// import { LoginForm } from "@/components/AuthForms/LoginForm";
+import { useState } from "react";
+
+export default function Authorization() {
+
+	const [activeTab, setActiveTab] = useState("login")
+  return (
+    <section className={styles.container}>
+      <div className={styles.tabs}>
+        <div
+          onClick={() => {
+            setActiveTab("login");
+          }}
+          className={
+            activeTab === "login"
+              ? `${styles.tab} ${styles.tab_active}`
+              : styles.tab
+          }
+        >
+          {" "}
+          Войти
+        </div>
+        <div
+          onClick={() => {
+            setActiveTab("signin");
+          }}
+          className={
+            activeTab === "signin"
+              ? `${styles.tab} ${styles.tab_active}`
+              : styles.tab
+          }
+        >
+          Зарегистрироваться
+        </div>
+      </div>
+
+      {/* {activeTab === "login" ? (
+        <>
+          <h2 className={styles.title}>Войти</h2>
+          <LoginForm />
+        </>
+      ) : (
+        <>
+          <h2 className={styles.title}>Регистрация</h2>
+          <RegisterForm />
+        </>
+      )} */}
+    </section>
+  );
+}
