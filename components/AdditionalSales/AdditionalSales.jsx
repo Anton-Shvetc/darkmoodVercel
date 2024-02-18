69364c731fd45b01a817b240d915a78c56e6ce3b
Anton <antonshvetswork@yandex.ru>
2024-02-18 19:38:30 +0300

    Добавлен слайдер к блоку - рекомендуем к покупке

--- components/AdditionalSales/AdditionalSales.jsx
+++ components/AdditionalSales/AdditionalSales.jsx
@@ -1,13 +1,24 @@
 "use client";
 import { AdditionalSalesCard } from "../AdditionalSalesCard/AdditionalSalesCard";
 import styles from "./AdditionalSales.module.scss";
-import imageUrl from "@/public/images/card-img.png";
+// import imageUrl from "@/public/images/card-img.png";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
+import { MdArrowForward } from "react-icons/md";
+import { MdArrowBack } from "react-icons/md";
+
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 export const AdditionalSales = () => {
   const pathname = usePathname();
   const idPath = pathname.replace("/catalog/", "");
+
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
 
   // const mockAdditionalSalesData = [
   //   {
@@ -73,16 +84,70 @@ export const AdditionalSales = () => {
     getCardsData();
   }, []);
 
+  const handleDragStart = (e) => e.preventDefault();
+
+  const responsive = {
+    0: {
+      items: 1,
+    },
+    480: {
+      items: 2,
+    },
+    800: { items: 3 },
+    1024: {
+      items: 6,
+    },
+  };
+
+  const items = data.map((cardData) => (
+    <AdditionalSalesCard key={cardData.id} data={cardData} />
+  ));
+
   return (
     <div className={styles.additionalSales}>
       <h2 className={styles.additionalSales__title}>Рекомендуем к покупке</h2>
+      {domLoaded && (
+        <AliceCarousel
+          mouseTracking
+          disableDotsControls={true}
+          items={items}
+          responsive={responsive}
+          renderPrevButton={({ isDisabled }) => {
+            if (!isDisabled) {
+              return (
+                <p
+                  style={{
+                    fontSize: "50px",
+                    left: "-40px",
+                  }}
+                  className="p-4 absolute left-0 top-20"
+                >
+                  <MdArrowBack />
+                </p>
+              );
+            }
+          }}
+          renderNextButton={({ isDisabled }) => {
+            if (!isDisabled) {
+              return (
+                <p
+                  style={{ fontSize: "50px", right: "-40px" }}
+                  className="p-4  absolute right-0 top-20"
+                >
+                  <MdArrowForward />
+                </p>
+              );
+            }
+          }}
+        />
+      )}
 
-      <div className={styles.additionalSales__cards}>
+      {/* <div className={styles.additionalSales__cards}>
         {data &&
           data.map((cardData) => (
             <AdditionalSalesCard key={cardData.id} data={cardData} />
           ))}
-      </div>
+      </div> */}
     </div>
   );
 };
