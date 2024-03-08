c95819eaf3643b092bdb2ddaa57352adbe556cb3
Anton <antonshvetswork@yandex.ru>
2024-03-09 00:44:40 +0300

    Добавлен запрос на добавление персональных данных

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -55,10 +55,51 @@ export default function Form({ arrInput, nameData, isEdit }) {
     }
   }, [watch()]);
 
+
+  const savePersonalData = async(userData) => {
+
+    const userInfo = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : ''
+    const userId = userInfo ? userInfo?.id : ''
+
+
+
+    try{
+ 
+    const response = await fetch(
+      `https://darkmode-serve.ru/api/users/${userId}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization:
+            "Bearer 63e74db5f842896da84149d352ea13c224cb240781490ff12f574a960df9a33894190bc96d5a5fc11483876cf43cc0d682900d178466dec4afdda24df86930916d7c4eaeb620c766ff2eb4889158991490aa90b598e940ca6cd11d50d21179f6c0c3096510f83eb9d867abbaf3d97693c477735fb250af26014c044494064979",
+
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      }
+    );
+
+           console.log(response);
+
+    } catch(e) {
+      console.log(e)
+    }
+
+  }
+
   const onSubmit = (data) => {
     localStorage.setItem(nameData, JSON.stringify(data));
     setPreviousData(data);
-    console.log('sub', data);
+    // console.log("sub", data);
+    savePersonalData(data);
+    // {
+    //   "data": {
+    //     "firstName": "222222"
+    //     "lastName": "222222"
+    //     "patronymic": "222222"
+    //     "phone": "222222"
+    //   }
+    // }
   };
 
   return (
