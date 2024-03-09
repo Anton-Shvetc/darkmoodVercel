0e8486cb240cd05a3c13c53f08af856f2c91c44c
Антон Швец <a.shvec@santehnika-tut.ru>
2024-03-09 12:34:13 +0300

    Добвлен переход по ссылкам - рекомендовано к покупке

--- components/AdditionalSalesCard/AdditionalSalesCard.jsx
+++ components/AdditionalSalesCard/AdditionalSalesCard.jsx
@@ -1,11 +1,14 @@
 import styles from "./AdditionalSalesCard.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 export const AdditionalSalesCard = ({ data }) => {
+
+  console.log(data)
   const imageUrl =
     "https://darkmode-serve.ru" + data.attributes.images.data[0].attributes.url;
 
   return (
-    <div className={styles.additionalSalesCard}>
+    <Link href={`/catalog/${data.id}`} className={styles.additionalSalesCard}>
       <Image
         className={styles.additionalSalesCard__image}
         src={imageUrl}
@@ -19,6 +22,6 @@ export const AdditionalSalesCard = ({ data }) => {
       <h6 className={styles.additionalSalesCard__title}>
         {data.attributes.title}
       </h6>
-    </div>
+    </Link>
   );
 };
